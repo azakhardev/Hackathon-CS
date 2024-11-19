b1d9a29e4276860aeb7ca56754302ebf4e0965a2
Edems <57250017+Edems-DEV@users.noreply.github.com>
2024-11-19 12:27:50 +0100

    feat: load more button

---
+++ src/components/ButtonLoadMore.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "./ui/Button";
+import { Separator } from "./ui/separator";
+
+interface ButtonLoadMoreProps {
+  className?: string;
+  title?: string;
+}
+
+export default function ButtonLoadMore({
+  className = "",
+  title = "More",
+}: ButtonLoadMoreProps) {
+  return (
+    <div
+      className={`flex flex-row justify-center items-center overflow-hidden ${className}`}
+    >
+      <Separator className="flex-grow" />
+      <Button variant="outline">{title}</Button>
+      <Separator className="flex-grow" />
+    </div>
+  );
+}
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -9,67 +9,79 @@ import AutomationsTable from "../automations/automations/AutomationsTable";
 import H1 from "@/components/ui/typography/H1";
 import { IRunner } from "@/pages/runners/types/IRunner";
 import H2 from "@/components/ui/typography/H2";
+import { IAutomation } from "../automations/_shared/IAutomation";
+import { IProject } from "../projects/types/IProject";
+import { Divide } from "lucide-react";
+import ButtonLoadMore from "@/components/ButtonLoadmore";
 
 export default function HomePage() {
+  // API
+  const projectsQuery = useQuery({
+    queryKey: ["runners"],
+    queryFn: async () => await RunnerModel.getRunners(undefined, 3),
+  });
   const runnersQuery = useQuery({
     queryKey: ["runners"],
     queryFn: async () => await RunnerModel.getRunners(undefined, 3),
   });
-
-  if (runnersQuery.data && "error" in runnersQuery.data) {
-    const errorData = runnersQuery.data as IErrorMessage;
-    return <ErrorMessage errorMessage={errorData} />;
-  }
-
-  if (!runnersQuery.data && !runnersQuery.isLoading) {
-    const error: IErrorMessage = {
-      code: "500",
-      error: "Internal server error",
-      message: "Server responded with undefined",
-    };
-    return <ErrorMessage errorMessage={error}></ErrorMessage>;
-  }
+  const jobsQuery = useQuery({
+    queryKey: ["runners"],
+    queryFn: async () => await RunnerModel.getRunners(undefined, 3),
+  });
+  const automationsQuery = useQuery({
+    queryKey: ["runners"],
+    queryFn: async () => await RunnerModel.getRunners(undefined, 3),
+  });
+  const automationTypesQuery = useQuery({
+    queryKey: ["runners"],
+    queryFn: async () => await RunnerModel.getRunners(undefined, 3),
+  });
 
   return (
     <div className="flex flex-col gap-10">
       {/* <H1>Homepage</H1> */}
-      <div className="flex flex-col gap-6">
-        <H1 className="w-full text-center">Dev</H1>
-        <div>
-          <H2>Projects</H2>
-          <Loader isLoading={runnersQuery.isLoading}>
-            {/* <ProjectsTable projects={[]} /> */}
-            <RunnersTable runners={runnersQuery.data as IRunner[]} />
-          </Loader>
-        </div>
-        <div>
-          <H2>Runners</H2>
-          <Loader isLoading={runnersQuery.isLoading}>
-            <RunnersTable runners={runnersQuery.data as IRunner[]} />
-          </Loader>
-        </div>
-        <div>
-          <H2>Jobs</H2>
-          <Loader isLoading={runnersQuery.isLoading}>
-            {/* <JobsTable jobs={[]} /> */}
-            <RunnersTable runners={runnersQuery.data as IRunner[]} />
-          </Loader>
+      <div>
+        <H1 className="w-full mb-0 text-center">Dev</H1>
+        <div className="flex flex-col gap-6">
+          <div>
+            <H2>Projects</H2>
+            <Loader isLoading={projectsQuery.isLoading}>
+              {/* <ProjectsTable projects={projectsQuery.data} /> */}
+              <RunnersTable runners={projectsQuery.data as IRunner[]} />
+            </Loader>
+            <ButtonLoadMore className="mt-2" />
+          </div>
+          <div>
+            <H2>Runners</H2>
+            <Loader isLoading={runnersQuery.isLoading}>
+              <RunnersTable runners={runnersQuery.data as IRunner[]} />
+            </Loader>
+          </div>
+          <div>
+            <H2>Jobs</H2>
+            <Loader isLoading={jobsQuery.isLoading}>
+              {/* <JobsTable jobs={[]} /> */}
+              <RunnersTable runners={jobsQuery.data as IRunner[]} />
+            </Loader>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col gap-6">
-        <H1 className="w-full text-center">Ops</H1>
-        <div>
-          <H2>Automations</H2>
-          <Loader isLoading={runnersQuery.isLoading}>
-            {/* <AutomationsTable automations={[]} /> */}
-            <RunnersTable runners={runnersQuery.data as IRunner[]} />
-          </Loader>
-        </div>
-        <div>
-          <H2>Automation Types</H2>
-          <Loader isLoading={runnersQuery.isLoading}>
-            <RunnersTable runners={runnersQuery.data as IRunner[]} />
-          </Loader>
+      <div>
+        <H1 className="w-full mb-0 text-center">Ops</H1>
+        <div className="flex flex-col gap-6">
+          <div>
+            <H2>Automations</H2>
+            <Loader isLoading={automationsQuery.isLoading}>
+              {/* <AutomationsTable automations={[]} /> */}
+              <RunnersTable runners={automationsQuery.data as IRunner[]} />
+            </Loader>
+          </div>
+          <div>
+            <H2>Automation Types</H2>
+            <Loader isLoading={automationTypesQuery.isLoading}>
+              <RunnersTable runners={automationTypesQuery.data as IRunner[]} />
+            </Loader>
+          </div>
         </div>
       </div>
     </div>
