9e4f3ee133130b6e13f93ac664f91e073e491e69
Edems <57250017+Edems-DEV@users.noreply.github.com>
2024-11-24 09:22:03 +0100

    fix: disable breadcrumbs for now

--- src/pages/_layout/_Layout.tsx
+++ src/pages/_layout/_Layout.tsx
@@ -7,15 +7,6 @@ import {
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/pages/_layout/app-sidebar";
 import Breadcrumbs from "@/components/ui/Breadcrumbs";
-// import {
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   BreadcrumbLink,
-//   BreadcrumbList,
-//   BreadcrumbPage,
-//   BreadcrumbSeparator,
-// } from "@/components/ui/breadcrumb";
-// import { Separator } from "@radix-ui/react-separator";
 
 export default function Layout() {
   //{ children }: { children: React.ReactNode }
@@ -23,9 +14,9 @@ export default function Layout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className=" bg-bg_secondary">
-        <header className="mb-6 border-b flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        {/* <header className="mb-6 border-b flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <Breadcrumbs />
-        </header>
+        </header> */}
         <main className="h-full max-w-full px-4 mt-4 mb-3 sm:px-6 md:px-12 lg:px-24 xl:px-32 sm:mt-6 md:mt-8 lg:mt-10 sm:mb-4 md:mb-5 bg-bg_secondary">
           <div className="max-w-screen-lg mx-auto">
             <Outlet />
